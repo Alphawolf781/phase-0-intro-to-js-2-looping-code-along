d3c506b0827ff77ef2c20744c17ce1c1e59f15cb
Alphawolf781 <vincent.wambua@student.moringaschool.com>
2024-06-30 16:40:46 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+
+let names = ["Charlie", "Samip", "Ali"];
+  let event = "birthday";
+  let messages = writeCards(names);
+  
+function writeCards(namesArray, eventName) {
+    
+    let thankYouMessages = [];
+  
+    
+    for (let i = 0; i < namesArray.length; i++) {
+      
+      let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+  }
+  
+  
+  console.log(messages);
+
+
+
+  function countDown(num){
+while(num>=0){
+    console.log(num)
+    num--
+}
+}
+
+countDown(10)
+    
+  
+  
